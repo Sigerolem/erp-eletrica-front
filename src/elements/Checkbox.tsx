19a20fc9c1b15ae86f20c9471f5c7a7eb75c949f
Regis Melo de Oliveira <regisoliveiramelo@gmail.com>
2025-11-10 15:14:34 -0300

    new checkbox element

---
+++ src/elements/Checkbox.tsx
@@ -0,0 +1,39 @@
+//checkbox input with state
+import { type InputHTMLAttributes, type TargetedEvent } from "preact";
+import type { Dispatch, StateUpdater } from "preact/hooks";
+
+interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  checked: boolean;
+  setChecked: Dispatch<StateUpdater<boolean>>;
+}
+
+export function Checkbox({
+  label,
+  name,
+  checked,
+  setChecked,
+  ...rest
+}: CheckboxProps) {
+  return (
+    <div className="flex flex-col gap-1 items-start">
+      <label htmlFor={name} className={"font-semibold"}>
+        {label}
+      </label>
+      <input
+        type="checkbox"
+        id={name}
+        name={name}
+        checked={checked}
+        onChange={(e) => {
+          setChecked(e.currentTarget.checked);
+        }}
+        className={
+          "min-w-8 min-h-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+        }
+        {...rest}
+      />
+    </div>
+  );
+}
