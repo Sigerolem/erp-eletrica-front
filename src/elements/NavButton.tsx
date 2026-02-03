822ce40646ed96d5be6dcb702cb7f1d2f98c57ce
Régis Melo <regisoliveiramelo@gmail.com>
2026-02-03 17:49:07 -0300

    deixa o site feio quando é dev pra facilitar minha vida

--- src/elements/NavButton.tsx
+++ src/elements/NavButton.tsx
@@ -2,17 +2,22 @@ import { useEffect, useState } from "preact/hooks";
 
 export function NavButton({ name, path }: { name: string; path: string }) {
   const [isSelected, setIsSelected] = useState(false);
+  const [isDev, setIsDev] = useState(false);
   useEffect(() => {
     const url = new URL(window.location.href);
     if (url.pathname == path) {
       setIsSelected(true);
     }
+    if (url.hostname.includes("localhost")) {
+      setIsDev(true);
+    }
   }, []);
   return (
     <a href={path}>
       <span
-        class={`block text-white ${isSelected ? "bg-slate-600" : ""
-          } cursor-pointer py-2 px-1 text-lg rounded-md hover:brightness-80`}
+        class={`block text-white ${
+          isSelected ? "bg-slate-600" : ""
+        } cursor-pointer py-2 px-1 text-lg rounded-md hover:brightness-80 ${isDev ? "bg-red-600" : ""}`}
       >
         {name}
       </span>
