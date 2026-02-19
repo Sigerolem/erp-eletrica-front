eec1997f7962a736379d904f61847fffb98fd829
Régis Melo <regisoliveiramelo@gmail.com>
2026-02-19 10:51:19 -0300

    notify errors on supplier creation

--- src/components/suppliers/CreateSupplierModal.tsx
+++ src/components/suppliers/CreateSupplierModal.tsx
@@ -1,4 +1,9 @@
-import { useEffect, type Dispatch, type StateUpdater } from "preact/hooks";
+import {
+  useEffect,
+  useState,
+  type Dispatch,
+  type StateUpdater,
+} from "preact/hooks";
 import { fetchWithToken } from "src/utils/fetchWithToken";
 import { SupplierDataForm } from "./SupplierDataForm";
 import type { SuppliersType } from "./Suppliers";
@@ -11,6 +16,8 @@ export function CreateSupplierModal({
   closeModal: () => void;
   setSupplier: Dispatch<StateUpdater<SuppliersType[]>>;
 }) {
+  const [isFetching, setIsFetching] = useState(false);
+
   useEffect(() => {
     const handleKeyPress = (e: KeyboardEvent) => {
       if (e.key == "Escape") {
@@ -26,24 +33,34 @@ export function CreateSupplierModal({
   }, []);
 
   async function handleDataSubmition(supplierData: Omit<SuppliersType, "id">) {
+    setIsFetching(true);
     const { code, data } = await fetchWithToken<{ supplier: SuppliersType }>({
       path: "/suppliers/create",
       method: "POST",
       body: JSON.stringify(supplierData),
     });
+    setIsFetching(false);
 
     if (code == 409) {
       let errors = {} as { [key: string]: string };
-      if (data.message.includes("entity.name")) {
+      if (data.message.includes("suppliers_name")) {
         errors = { ...errors, name: "Esse nome já foi utilizado" };
-      } else if (data.message.includes("entity.cnpj")) {
+        window.alert(`O nome ${supplierData.name} já está em uso.`);
+      } else if (data.message.includes("suppliers_cnpj")) {
         errors = { ...errors, cnpj: "Esse CNPJ já foi cadastrado" };
+        window.alert(`O CNPJ ${supplierData.cnpj} já está em uso.`);
+      } else {
+        window.alert(
+          "Erro de conflito inesperado ao salvar. Contate o desenvolvedor se necessário.",
+        );
       }
       return errors;
     }
 
     if (code == 400) {
-      window.alert("Requisição feita ao servidor é inválida.");
+      window.alert(
+        "Requisição feita ao servidor é inválida. Verifique os campos preenchidos.",
+      );
       console.error(code, data);
     }
 
@@ -51,6 +68,7 @@ export function CreateSupplierModal({
       setSupplier((prev) => [data.supplier, ...prev]);
       closeModal();
     } else {
+      window.alert(`Erro inesperado ao salvar fornecedor. Código: ${code}`);
       console.error(code, data);
     }
   }
@@ -82,7 +100,7 @@ export function CreateSupplierModal({
         </header>
         <div>
           <SupplierDataForm doOnSubmit={handleDataSubmition}>
-            <Button type={"submit"} text="Salvar" />
+            <Button type={"submit"} text="Salvar" disabled={isFetching} />
           </SupplierDataForm>
         </div>
       </div>
